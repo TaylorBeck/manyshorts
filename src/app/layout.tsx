e0d5f85fe8c07cbfd6fa3b2f805df9f26e877213
Taylor Beck <beck.taylorg@gmail.com>
2024-10-14 07:46:57 -0700

    Update layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,16 @@
 import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import { Providers } from '@/components/Providers';
 import { Toaster } from '@/components/ui/toaster';
+import { Providers } from '@/components/Providers';
+import { Sidebar } from '@/components/Sidebar';
+import { Appbar } from '@/components/Appbar';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'ManyShorts',
-  description: 'Create and share AI-generated YouTube Shorts'
+  description: 'Create and share AI generated YouTube Shorts'
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
@@ -16,9 +18,17 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <body className={inter.className}>
         <Providers>
-          <div className="flex flex-col !min-h-screen">
-            <main className="flex-grow">{children}</main>
-            <Toaster />
+          <Toaster />
+          <div className="flex h-screen bg-gray-100">
+            <Sidebar />
+
+            {/* Main content */}
+            <div className="flex-1 flex flex-col">
+              <Appbar />
+
+              {/* Page content */}
+              <main className="flex-1 p-8 overflow-auto">{children}</main>
+            </div>
           </div>
         </Providers>
       </body>
